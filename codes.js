b774900d78462edcce0f54a9b55ba5ca60dc3a1d
vaibhav <vaibhav123465789@gmail.com>
2026-06-27 13:11:11 +0530

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,24 @@
+   //console.log('you have chose bat');   
+    //let randomNumber = Math.random () * 3
+    //let computerChoice;
+    //if (randomNumber > 0 && randomNumber <= 1) {
+     //computerChoice = 'bat';
+      //console.log('computer choice is bat');
+    //}else if(randomNumber > 1 && randomNumber <= 2){
+     //computerChoice='Ball';
+      //  console.log('computer choice is Ball');
+
+    //}else{
+         computerChoice ='stump';
+       // console.log('computer choice is Stump');
+    //}
+    //console.log(`computer choice is ${computerChoice}`);
+    //if(computerChoice === 'Ball'){
+      //  console.log(`User is win the match`);
+    //}else if (computerChoice === 'bat'){
+      //  console.log(`ti's is a tie`);
+    //}else if(computerChoice === 'stump'){
+      //  console.log(`computer is win`);
+    //}
+    
+    
